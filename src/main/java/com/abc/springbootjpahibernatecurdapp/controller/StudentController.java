55ecf4fa556e5a2311f195be255ed518dbf7ac24
Kumar Vijay <vijay81238@gmail.com>
2023-05-25 13:28:20 +0530

    Added get student api

--- src/main/java/com/abc/springbootjpahibernatecurdapp/controller/StudentController.java
+++ src/main/java/com/abc/springbootjpahibernatecurdapp/controller/StudentController.java
@@ -6,10 +6,9 @@ import com.abc.springbootjpahibernatecurdapp.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,5 +22,11 @@ public class StudentController {
         Student newStudent = studentService.saveStudent(inputStudent);
 
         return ResponseEntity.status(HttpStatus.OK).body(newStudent);
+    }
+
+    @GetMapping("/getAllStudent")
+    public ResponseEntity<List<Student>> getStudents() {
+        List<Student> allStudent = studentService.getAllStudents();
+        return ResponseEntity.status(HttpStatus.OK).body(allStudent);
     }
 }
